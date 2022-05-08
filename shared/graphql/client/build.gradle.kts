24b544f9d1d3f12c8b74a413a41bc03f2c86b28a
Masatoshi Kubode <masatoshi@wantedly.com>
2022-05-09 02:15:09 +0900

    Inject GITHUB_TOKEN

--- shared/graphql/client/build.gradle.kts
+++ shared/graphql/client/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
+import org.jetbrains.kotlin.konan.properties.loadProperties
+
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
@@ -15,3 +18,41 @@ kotlin {
         implementation(libs.bundles.apollo.testing)
     }
 }
+
+// region: Generate GITHUB_TOKEN
+val generatedGithubTokenSourceDir = file("$buildDir/generated/github-token")
+
+kotlin {
+    sourceSets {
+        getByName("commonMain") {
+            kotlin.srcDirs(generatedGithubTokenSourceDir)
+        }
+    }
+}
+
+val generateGithubToken by tasks.registering {
+    val packageName = "com.github.kubode.shared.graphql.client"
+    val fileName = "GeneratedGitHubToken.kt"
+    val propName = "generatedGitHubToken"
+    val githubToken = loadProperties("$rootDir/local.properties").getProperty("GITHUB_TOKEN")
+
+    inputs.property(propName, githubToken)
+    outputs.dir(generatedGithubTokenSourceDir)
+
+    doLast {
+        generatedGithubTokenSourceDir.delete()
+        val outputDir = File(generatedGithubTokenSourceDir, packageName.replace('.', File.separatorChar))
+        mkdir(outputDir)
+
+        File(outputDir, fileName).writeText(
+            """
+                package $packageName
+                
+                internal const val GITHUB_TOKEN = "$githubToken"
+            """.trimIndent()
+        )
+    }
+}
+
+tasks.withType<KotlinCompile<*>>().all { dependsOn(generateGithubToken) }
+// endregion
---
+++ shared/graphql/client/src/commonMain/kotlin/com/github/kubode/shared/graphql.client/GraphqlClient.kt
@@ -0,0 +1,29 @@
+package com.github.kubode.shared.graphql.client
+
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.api.http.HttpRequest
+import com.apollographql.apollo3.api.http.HttpResponse
+import com.apollographql.apollo3.network.http.HttpInterceptor
+import com.apollographql.apollo3.network.http.HttpInterceptorChain
+import com.apollographql.apollo3.network.http.HttpNetworkTransport
+import com.apollographql.apollo3.network.http.LoggingInterceptor
+
+private class AuthorizationInterceptor : HttpInterceptor {
+    override suspend fun intercept(request: HttpRequest, chain: HttpInterceptorChain): HttpResponse {
+        return chain.proceed(
+            request.newBuilder()
+                .addHeader("Authorization", "bearer $GITHUB_TOKEN")
+                .build()
+        )
+    }
+}
+
+val apolloClient: ApolloClient = ApolloClient.Builder()
+    .serverUrl("https://api.github.com/graphql")
+    .networkTransport(
+        HttpNetworkTransport.Builder()
+            .addInterceptor(AuthorizationInterceptor())
+            .addInterceptor(LoggingInterceptor())
+            .build()
+    )
+    .build()
