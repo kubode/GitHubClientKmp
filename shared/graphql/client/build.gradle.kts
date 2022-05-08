1255ff393563be08d5dc1337f2eebd3082e35e32
Masatoshi Kubode <masatoshi@wantedly.com>
2022-05-08 23:41:48 +0900

    Add dependencies

--- shared/graphql/client/build.gradle.kts
+++ shared/graphql/client/build.gradle.kts
@@ -8,8 +8,10 @@ configureKotlinMultiplatformProjectWithDefault()
 kotlin {
     sourceSets["commonMain"].dependencies {
         api(libs.apollo.runtime)
+        api(libs.apollo.normalized.cache)
     }
     sourceSets["commonTest"].dependencies {
         implementation(libs.kotlin.test)
+        implementation(libs.bundles.apollo.testing)
     }
 }
