ba3ae70421cca3f4436debab0711d3b2d52e34e2
Masatoshi Kubode <masatoshi@wantedly.com>
2022-05-15 23:19:31 +0900

    Tweaks

--- androidApp/src/main/java/com/github/kubode/githubclient/android/MainActivity.kt
+++ androidApp/src/main/java/com/github/kubode/githubclient/android/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.kubode.githubclient.android.ui.theme.GitHubKMMTheme
+import com.github.kubode.githubclient.shared.feature.search.Greeting
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +22,7 @@ class MainActivity : ComponentActivity() {
             GitHubKMMTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting(Greeting().greeting())
                 }
             }
         }
@@ -28,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp))
 }
 
 @Preview(showBackground = true)
