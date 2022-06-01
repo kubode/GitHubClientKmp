e88893d298f201ff35d0b891be0185f6345e7436
Masatoshi Kubode <masatoshi@wantedly.com>
2022-06-02 00:55:31 +0900

    Create a ViewModel

--- shared/feature/search/build.gradle.kts
+++ shared/feature/search/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 kotlin {
     sourceSets["commonMain"].dependencies {
-        implementation(projects.shared.graphql.schema)
+        implementation(projects.shared.graphql.client)
         api(compose.ui)
         api(compose.foundation)
         api(compose.material)
--- shared/feature/search/src/commonMain/kotlin/com/github/kubode/githubclient/shared/feature/search/Search.kt
+++ shared/feature/search/src/commonMain/kotlin/com/github/kubode/githubclient/shared/feature/search/Search.kt
@@ -1,9 +1,34 @@
 package com.github.kubode.githubclient.shared.feature.search
 
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.cache.normalized.watch
 
 @Composable
-fun Search() {
+fun RepositorySearchScreen(viewModel: RepositorySearchViewModel) {
     Text("Search")
+}
+
+class RepositorySearchViewModel(
+    private val apolloClient: ApolloClient
+) {
+    var state: State by mutableStateOf(State())
+        private set
+
+    data class State(
+        val pages: List<Page> = emptyList()
+    ) {
+        sealed class Page {
+            object Loading : Page()
+            data class Success(val data: SearchRepositoriesQuery.Data)
+            data class Failure(val throwable: Throwable)
+        }
+    }
+
+    fun initialLoad() {
+        // TODO
+        apolloClient.query(SearchRepositoriesQuery("Kotlin"))
+            .watch()
+    }
 }
