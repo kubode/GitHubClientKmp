41e0d63d2886623439cda44ecca4e2d350f635e8
Masatoshi Kubode <masatoshi@wantedly.com>
2022-06-02 00:07:31 +0900

    Fix iOS build

--- shared/umbrella/src/iosMain/kotlin/Dummy.kt
+++ shared/umbrella/src/iosMain/kotlin/Dummy.kt
@@ -1,8 +1,10 @@
 import androidx.compose.ui.window.Application
 import com.github.kubode.githubclient.shared.feature.search.Search
+import platform.UIKit.UIViewController
 
 // This file is required because the header file will not be created if any source code does not exist on K/N.
-fun provideApplication(): Application {
+
+fun provideApplication(): UIViewController {
     return Application("GitHub") {
         Search()
     }
