dc81070820f47c8e6fe074ad3c5c4b7106ab9081
Masatoshi Kubode <swi.masatoshi@gmail.com>
2022-10-12 23:18:29 +0900

    Remove unnecessary configs

--- build-logic/convention/src/main/kotlin/githubclient.multiplatform.gradle.kts
+++ build-logic/convention/src/main/kotlin/githubclient.multiplatform.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
-
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
@@ -13,18 +11,6 @@ kotlin {
     // TODO: Move to sourceSets {} and dependsOn(iosMain)
     sourceSets["iosSimulatorArm64Main"].dependsOn(sourceSets["iosMain"])
     sourceSets["iosSimulatorArm64Test"].dependsOn(sourceSets["iosTest"])
-
-    // opt-in @OptIn
-    sourceSets.all {
-        languageSettings {
-            optIn("kotlin.RequiresOptIn")
-        }
-    }
-
-    // Exports KDoc to iOS
-    targets.withType<KotlinNativeTarget> {
-        compilations["main"].kotlinOptions.freeCompilerArgs += "-Xexport-kdoc"
-    }
 }
 
 // Workaround: https://github.com/JetBrains/compose-jb/pull/2164
