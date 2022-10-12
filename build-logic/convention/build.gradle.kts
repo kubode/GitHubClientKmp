490910b49380e9e76794a73436b7f3f360c01532
Masatoshi Kubode <swi.masatoshi@gmail.com>
2022-10-12 22:52:05 +0900

    Remove unnecessary configus

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -2,13 +2,6 @@ plugins {
     `kotlin-dsl`
 }
 
-group = "com.github.kubode.githubclient.buildlogic"
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
-
 dependencies {
     implementation(libs.android.gradle.plugin)
     implementation(libs.kotlin.gradle.plugin)
