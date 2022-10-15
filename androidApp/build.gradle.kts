af2a00ecbcb87e65999dfcc728afe534885ae702
Masatoshi Kubode <swi.masatoshi@gmail.com>
2022-10-15 19:06:11 +0900

    Move android.application configs to build-logic

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,26 +1,6 @@
 plugins {
-    id("com.android.application")
-    kotlin("android")
+    id("githubclient.android.application")
     alias(libs.plugins.compose.multiplatform)
-}
-
-android {
-    compileSdk = 33
-    defaultConfig {
-        applicationId = "com.github.kubode.githubclient.android"
-        minSdk = 26
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    namespace = "com.github.kubode.githubclient.android"
 }
 
 dependencies {
