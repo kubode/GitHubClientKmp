f174ab47571fdabdc14d1bca1f765b0f23ea51a8
Masatoshi Kubode <swi.masatoshi@gmail.com>
2022-10-15 19:14:52 +0900

    Move multiplatform.android config to multiplatform

--- build-logic/convention/src/main/kotlin/githubclient.multiplatform.gradle.kts
+++ build-logic/convention/src/main/kotlin/githubclient.multiplatform.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("multiplatform")
-    id("githubclient.multiplatform.android")
+    id("com.android.library")
 }
 
 kotlin {
@@ -11,4 +11,18 @@ kotlin {
     // TODO: Move to sourceSets {} and dependsOn(iosMain)
     sourceSets["iosSimulatorArm64Main"].dependsOn(sourceSets["iosMain"])
     sourceSets["iosSimulatorArm64Test"].dependsOn(sourceSets["iosTest"])
+}
+
+android {
+    // e.g. :shared:feature:search -> com.github.kubode.githubclient.shared.feature.search
+    namespace = "${project.group}${project.path.replace(":", ".").replace("-", "")}"
+
+    compileSdk = COMPILE_SDK
+    defaultConfig {
+        minSdk = MIN_SDK
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    // Move AndroidManifest.xml from src/main to src/androidMain
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
 }
