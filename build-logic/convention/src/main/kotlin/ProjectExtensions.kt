4c0842be8f3e60aca8c254a2071133679dfc954e
Masatoshi Kubode <swi.masatoshi@gmail.com>
2023-08-03 23:11:55 +0900

    Add some extensions

--- build-logic/convention/src/main/kotlin/ProjectExtensions.kt
+++ build-logic/convention/src/main/kotlin/ProjectExtensions.kt
@@ -5,3 +5,8 @@ import org.gradle.kotlin.dsl.getByType
 
 internal val Project.libs
     get(): VersionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+internal fun VersionCatalog.version(alias: String) = findVersion(alias).get()
+internal fun VersionCatalog.library(alias: String) = findLibrary(alias).get()
+internal fun VersionCatalog.bundle(alias: String) = findBundle(alias).get()
+internal fun VersionCatalog.plugin(alias: String) = findPlugin(alias).get()
